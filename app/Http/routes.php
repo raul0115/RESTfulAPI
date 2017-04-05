ccc58a4a32bb1f7d7af60b7cc46c5cada5c4a651
raul0115 <raul0115@gmail.com>
2017-04-05 17:41:54 -0400

    Se crean los primeros controladores y routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('fabricantes','FabricanteController');
+Route::resource('fabricantes.vehiculos','FabricanteVehiculoController',['except'=>['show']]);
+Route::resource('vehiculos','VehiculoController',['only'=>['index','show']]);
+
